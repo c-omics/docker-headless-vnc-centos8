65e59b6c3467f66d210fde00dcd4ecbbb3d9b2fe
Ian Merrick <MerrickI@Cardiff.ac.uk>
2020-07-13 15:06:07 +0100

    Added R/rstudio to the image

--- src/common/install/modules.sh
+++ src/common/install/modules.sh
@@ -5,7 +5,21 @@
 yum install -y environment-modules make
 chmod 666 /usr/share/Modules/init/.modulespath
 yum group install -y "Development tools"
-yum install -y gcc openmpi openmpi-devel mpich mpich-devel lmdb-libs
+yum install -y gcc \
+	       openmpi \
+	       openmpi-devel \
+	       mpich \
+	       mpich-devel \
+	       lmdb-libs
+dnf install -y epel-release
+dnf config-manager --set-enabled PowerTools
+yum install -y \
+	       R
+	       openssl \
+	       openssl-libs \
+	       openssl-devel \
+	       libglvnd-egl
+dnf install -y https://download1.rstudio.org/desktop/centos8/x86_64/rstudio-1.3.959-x86_64.rpm
 yum make clean
 rm -rf /var/yum/cache
 
